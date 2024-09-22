b1c8a48f63df62337add21e73e823df8b7ec6d63
carrillo21108 <car21108@uvg.edu.gt>
2024-09-22 08:28:37 -0600

    feat: Add performance measurement with execution time logging

--- bruteforce_seq.c
+++ bruteforce_seq.c
@@ -1,15 +1,18 @@
 #include <string.h>
 #include <stdio.h>
-#include <openssl/des.h>
+#include <stdlib.h>
+#include <openssl/des.h> 
+#include <time.h>  // Para medir el tiempo de ejecución
 
 void decrypt(long key, char *ciph, int len) {
+    
     DES_key_schedule schedule;
     DES_cblock des_key;
     memset(des_key, 0, 8); 
     memcpy(des_key, &key, sizeof(long)); 
     DES_set_key_unchecked(&des_key, &schedule);  
 
-    for (int i = 0; i < len; i += 8) {
+    for (int i = 0; i < len; i += 8) {  
         DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &schedule, DES_DECRYPT);
     }
 }
@@ -17,9 +20,9 @@ void decrypt(long key, char *ciph, int len) {
 void encrypt(long key, char *ciph, int len) {
     DES_key_schedule schedule;
     DES_cblock des_key;
-    memset(des_key, 0, 8); 
-    memcpy(des_key, &key, sizeof(long)); 
-    DES_set_key_unchecked(&des_key, &schedule);  
+    memset(des_key, 0, 8);
+    memcpy(des_key, &key, sizeof(long));
+    DES_set_key_unchecked(&des_key, &schedule);
 
     for (int i = 0; i < len; i += 8) {
         DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &schedule, DES_ENCRYPT);
@@ -39,13 +42,33 @@ int tryKey(long key, char *ciph, int len) {
 unsigned char cipher[] = {108, 245, 65, 63, 125, 200, 150, 66, 17, 170, 207, 170, 34, 31, 70, 215, 0};
 
 int main() {
+    long upper = (1L << 56);  // (2^56)
     int ciphlen = strlen((const char *)cipher);
-    for (long i = 0; i < (1L << 56); ++i) {
+    long found = 0;
+
+    // Medición del tiempo de inicio
+    clock_t start_time = clock();
+
+    for (long i = 0; i < upper; ++i) {
         if (tryKey(i, (char *)cipher, ciphlen)) {
-            printf("Key found: %li\n", i);
+            found = i;
             break;
         }
     }
+
+    // Medición del tiempo final
+    clock_t end_time = clock();
+    double time_spent = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    if (found) {
+        decrypt(found, (char *)cipher, ciphlen);
+        printf("Key found: %li, Decrypted text: %s\n", found, cipher);
+    } else {
+        printf("Key not found.\n");
+    }
+
+    // Imprimir el tiempo transcurrido
+    printf("Time taken: %.2f seconds\n", time_spent);
+
     return 0;
 }
-
