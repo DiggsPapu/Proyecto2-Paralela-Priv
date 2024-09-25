b24cd1ef7dd30ba2ca45ec18bb23bd75a65955c4
carrillo21108 <car21108@uvg.edu.gt>
2024-09-25 09:18:25 -0600

    feat: Add encryption and decryption functions with padding

--- bruteforce_alternative_1.c
+++ bruteforce_alternative_1.c
@@ -5,6 +5,54 @@
 #include <openssl/des.h>
 #include <time.h>
 
+void add_padding(unsigned char *input, int *len) {
+    int padding = 8 - (*len % 8);
+    for (int i = 0; i < padding; ++i) {
+        input[*len + i] = padding;
+    }
+    *len += padding;
+}
+
+void remove_padding(unsigned char *input, int *len) {
+    int padding = input[*len - 1];
+    *len -= padding;
+}
+
+void decrypt_message(long key, unsigned char *ciph, int *len) {
+    DES_cblock keyBlock;
+    DES_key_schedule schedule;
+
+    for (int i = 0; i < 8; ++i) {
+        keyBlock[i] = (key >> (i * 8)) & 0xFF;
+    }
+
+    DES_set_odd_parity(&keyBlock);
+    DES_set_key_checked(&keyBlock, &schedule);
+
+    for (int i = 0; i < *len; i += 8) {
+        DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &schedule, DES_DECRYPT);
+    }
+    remove_padding(ciph, len);
+}
+
+void encrypt_message(long key, unsigned char *ciph, int *len) {
+    DES_cblock keyBlock;
+    DES_key_schedule schedule;
+
+    for (int i = 0; i < 8; ++i) {
+        keyBlock[i] = (key >> (i * 8)) & 0xFF;
+    }
+
+    DES_set_odd_parity(&keyBlock);
+    DES_set_key_checked(&keyBlock, &schedule);
+
+    add_padding(ciph, len);
+
+    for (int i = 0; i < *len; i += 8) {
+        DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &schedule, DES_ENCRYPT);
+    }
+}
+
 int main(int argc, char *argv[]) {
     MPI_Comm comm = MPI_COMM_WORLD;
     MPI_Init(&argc, &argv);
