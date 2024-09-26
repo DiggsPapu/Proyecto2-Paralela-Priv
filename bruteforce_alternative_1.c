025c97b2512f43f9dea87b2c2aa13a9a55d92515
carrillo21108 <car21108@uvg.edu.gt>
2024-09-25 18:49:00 -0600

    feat: Add key search functionality and message decryption

--- bruteforce_alternative_1.c
+++ bruteforce_alternative_1.c
@@ -53,10 +53,35 @@ void encrypt_message(long key, unsigned char *ciph, int *len) {
     }
 }
 
+int tryKey(long key, unsigned char *ciph, int len, const char *search) {
+    unsigned char temp[len + 1];
+    memcpy(temp, ciph, len);
+    temp[len] = 0; // Ensure the string is null-terminated
+
+    decrypt_message(key, temp, &len);
+    temp[len] = '\0'; // Explicitly null-terminate the decrypted string
+
+    if (strstr((char *)temp, search) != NULL) {
+        return 1; // Key found
+    }
+    return 0; // Key not found
+}
+
 int main(int argc, char *argv[]) {
     MPI_Comm comm = MPI_COMM_WORLD;
     MPI_Init(&argc, &argv);
     
+    long key = 1234567890L;
+    char *search = "test";
+    
+    // Dummy encrypted message (for example purposes)
+    unsigned char cipher[8] = {0x85, 0x83, 0x87, 0xA1, 0xB0, 0xA2, 0x98, 0x93};
+    int len = 8;
+
+    if (tryKey(key, cipher, len, search)) {
+        printf("Key %li found!\n", key);
+    }
+
     MPI_Finalize();
     return EXIT_SUCCESS;
 }
